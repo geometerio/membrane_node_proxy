73f5db7c4bc337be50d4f14cc16bb14fef5b4de4
Eric Saxby <sax@geometer.io>
2021-09-30 09:34:18 -0700

    Greatly simplify check for whether :gen_udp can use :socket backend

--- lib/membrane_node_proxy/config.ex
+++ lib/membrane_node_proxy/config.ex
@@ -10,42 +10,11 @@ defmodule Membrane.NodeProxy.Config do
 
   @spec update() :: :ok
   def update() do
-    if Application.get_env(:membrane_node_proxy, :use_inet_socket?) |> is_nil() do
-      with {:ok, libraries} <- libs(),
-           {:ok, kernel} <- libraries |> kernel_version() do
-        Application.put_env(:membrane_node_proxy, :use_inet_socket?, supports_socket?(kernel))
-      else
-        _error ->
-          Application.put_env(:membrane_node_proxy, :use_inet_socket?, false)
-      end
-    end
+    if Application.get_env(:membrane_node_proxy, :use_inet_socket?) |> is_nil(),
+      do: Application.put_env(:membrane_node_proxy, :use_inet_socket?, supports_socket?())
 
     :ok
   end
 
-  defp supports_socket?(kernel_version) do
-    normalized_version =
-      case String.split(kernel_version, ".") do
-        [_major, _minor] -> kernel_version <> ".0"
-        [_major] -> kernel_version <> ".0.0"
-        _other -> kernel_version
-      end
-
-    Version.match?(normalized_version, ">= 8.1.0")
-  end
-
-  defp kernel_version(["kernel-" <> version | _rest]),
-    do: {:ok, version}
-
-  defp kernel_version([]),
-    do: {:error, :enoent}
-
-  defp kernel_version([_ | rest]),
-    do: kernel_version(rest)
-
-  defp libs(),
-    do:
-      :code.root_dir()
-      |> Path.join("lib")
-      |> File.ls()
+  defp supports_socket?(), do: Code.ensure_loaded?(:gen_udp_socket)
 end
